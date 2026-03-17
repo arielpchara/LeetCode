e8d410d73e8b34f3bcd6264054cdae27520300f0
Ariel Pchara <ariel@pchara.com>
2026-03-17 14:55:00 -0300

    feat: implement twoSum function to find indices of two numbers that add up to target

--- easy/01-two-sum/index.ts
+++ easy/01-two-sum/index.ts
@@ -4,10 +4,15 @@
  * @returns Indices of the two matching values.
  */
 function twoSum(nums: number[], target: number): number[] | null {
-  void nums;
-  void target;
-  // TODO: implement
-  return null;
+  const store: Record<number, number> = {}
+  for (let i = 0; i< nums.length; i++) {
+    const complement = target - nums[i]
+    if( store[complement] != undefined ) {
+      return [store[complement], i]
+    }
+    store[nums[i]] = i
+  }
+  return null
 }
 
 export { twoSum };
