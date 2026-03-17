ef43be8bc4407d459bb37bdec9a8dd8ce1502ff0
Ariel Pchara <ariel@pchara.com>
2026-03-17 17:09:57 -0300

    feat: implement mergeTwoLists function to merge two sorted linked lists

--- easy/04-merge-two-sorted-lists/index.test.ts
+++ easy/04-merge-two-sorted-lists/index.test.ts
@@ -41,6 +41,10 @@ describe('Merge Two Sorted Lists', () => {
     expect(listToArray(mergeTwoLists(null, arrayToList([0])))).toEqual([0]);
   });
 
+  test('asymmetric list : [1,7,10] + [2,7,8,9,12] => [0]', () => {
+    expect(listToArray(mergeTwoLists(arrayToList([1,7,10]), arrayToList([2,7,8,9,12])))).toEqual([1,2,7,7,8,9,10,12]);
+  });
+
   test('[1,3,5] + [2,4,6] => [1,2,3,4,5,6]', () => {
     expect(listToArray(mergeTwoLists(arrayToList([1, 3, 5]), arrayToList([2, 4, 6])))).toEqual([1, 2, 3, 4, 5, 6]);
   });
--- easy/04-merge-two-sorted-lists/index.ts
+++ easy/04-merge-two-sorted-lists/index.ts
@@ -8,16 +8,36 @@ class ListNode {
   }
 }
 
+function listToArray(node: ListNode | null): number[] {
+  const result: number[] = [];
+  let current = node;
+
+  while (current) {
+    result.push(current.val);
+    current = current.next;
+  }
+
+  return result;
+}
+
 /**
  * @param list1 - First sorted list.
  * @param list2 - Second sorted list.
  * @returns Head of the merged list.
  */
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-  void list1;
-  void list2;
-  // TODO: implement
-  return null;
+  
+  if(list1 === null) return list2;
+  if(list2 === null) return list1;
+
+  if( list1.val <= list2.val ) {
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2
+  }
+  
 }
 
 export { mergeTwoLists, ListNode };
