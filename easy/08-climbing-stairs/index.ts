3578a3116250e6e233b18188efecd4bdefc78d02
Ariel Pchara <ariel@pchara.com>
2026-03-18 11:12:54 -0300

    feat: implement climbStairs function to calculate distinct ways to climb stairs

--- easy/08-climbing-stairs/index.ts
+++ easy/08-climbing-stairs/index.ts
@@ -2,10 +2,19 @@
  * @param n - Number of steps.
  * @returns Distinct ways to climb to the top.
  */
-function climbStairs(n: number): number | null {
-  void n;
-  // TODO: implement
-  return null;
+function climbStairs(n: number): number {
+  if(n < 1 || n >= 45) return 0
+  // 1 = 1
+  // 2 = 1,1 / 2
+  // 3 = 1,1,1 / 1,2 / 2,1
+  // 4 = 1,1,1,1 / 1,1,2 / 2,1,1 / 2,2
+  // 5 = 1,1,1,1,1 / 1,1,1,2 / 1,1,2,1 / 1,2,1,1 / 2,1,1,1 / 1,2,2 / 2,1,2 / 2,2,1
+  const seq: number[] = [1,1]
+  for(let i = 1; i < n; i++) {
+    seq[i+1] = seq[i] + seq[i-1]
+    console.log(seq)
+  }
+  return seq.pop() || 0;
 }
 
 export { climbStairs };
